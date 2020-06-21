7317ee9cde441d35d7b895266cbf2fd9f0285d01
l-404-l <saywhattowhat@gmail.com>
2020-06-21 01:55:05 -0500

    Cancer

--- SpeakerPhone/Main.cs
+++ SpeakerPhone/Main.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-[assembly: MelonModInfo(typeof(SpeakerPhone.Main), "Speaker Phone", "1.0.0", "404#0004", "https://github.com/l-404-l/")] 
+[assembly: MelonModInfo(typeof(SpeakerPhone.Main), "Speaker Phone", "1.0.0", "404#0004", "https://github.com/l-404-l/SpeakerPhone")] 
 [assembly: MelonModGame("VRChat", "VRChat")]
 
 namespace SpeakerPhone
@@ -21,19 +21,20 @@ namespace SpeakerPhone
             if (RoomManagerBase.field_Internal_Static_ApiWorld_0 == null || RoomManagerBase.field_Internal_Static_ApiWorldInstance_0 == null)
                 return;
 
-            if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.G))
-            {
-                SpeakerPhone = !SpeakerPhone;
-                if (SpeakerPhone)
-                    USpeaker.field_Internal_Static_Single_1 = float.MaxValue; // Local Speaker
-                else USpeaker.field_Internal_Static_Single_1 = 1f;
-            }
             if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.K))
             {
                 PublicSpeaker = !PublicSpeaker;
                 if (PublicSpeaker)
                     USpeaker.field_Internal_Static_Single_0 = float.MaxValue; // Players Speaker
                 else USpeaker.field_Internal_Static_Single_0 = 1f;
+            }
+
+            if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.G))
+            {
+                SpeakerPhone = !SpeakerPhone;
+                if (SpeakerPhone)
+                    USpeaker.field_Internal_Static_Single_1 = float.MaxValue; // Local Speaker
+                else USpeaker.field_Internal_Static_Single_1 = 1f;
             }
         }
     }
